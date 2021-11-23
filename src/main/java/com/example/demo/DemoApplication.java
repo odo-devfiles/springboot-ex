99f43519f7123245eda92082ae678264c6b21a1c
grippaluciano <grippa.luciano@gmail.com>
2021-11-23 09:12:31 +0000

    use env variable

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class DemoApplication {
 
+    @Value("${APP_TITLE}")
+    private String title;
+
     @RequestMapping("/")
     String home() {
-        return "Hello World! test da luciano grippa modificato il 23/11/2021 ore 09:51";
+        return String.format("title: %s message: %s",title, "Hello World! test da luciano grippa modificato il 23/11/2021 ore 09:51");
     }
 
     public static void main(String[] args) {
